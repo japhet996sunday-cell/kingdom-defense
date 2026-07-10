6a3e592e92587f1caa364afcb3ee298eeb48e4d3
Japhet Sunday <japhet996sunday@gmail.com>
2026-07-10 11:23:48 +0100

    Add Input class for mouse event handling
    Implement Input class to handle mouse events on canvas.

--- js/input.js
+++ js/input.js
@@ -1,1 +1,34 @@
+export class Input {
 
+    constructor(canvas) {
+
+        this.mouse = {
+            x: 0,
+            y: 0
+        };
+
+        this.clicked = false;
+
+        canvas.addEventListener("mousemove", (event) => {
+
+            const rect = canvas.getBoundingClientRect();
+
+            const scaleX = canvas.width / rect.width;
+            const scaleY = canvas.height / rect.height;
+
+            this.mouse.x = (event.clientX - rect.left) * scaleX;
+            this.mouse.y = (event.clientY - rect.top) * scaleY;
+
+        });
+
+        canvas.addEventListener("click", () => {
+            this.clicked = true;
+        });
+
+    }
+
+    resetClick() {
+        this.clicked = false;
+    }
+
+              }
